cb5b80cd9c91a730bd6b9731c43cbe7e81ff5df8
二三三的石头 <mcotf@outlook.com>
2025-04-18 11:37:10 +0800

    懒狗更新

---
+++ js/1.js
@@ -0,0 +1,13 @@
+// document.addEventListener('mousemove',function(event){
+// 	let x = event.clientX
+// 	let y = event.clientY;
+// 	let img = document.getElementById('背景图');
+// 	img.style.left = x - 'px';
+// 	img.style.top = y - 'px';
+// });
+// console.log('__    __     ______     ______     ______   ______');
+// console.log('/\ "-./  \   /\  ___\   /\  __ \   /\__  _\ /\  ___\ ');
+// console.log('\ \ \-./\ \  \ \ \____  \ \ \/\ \  \/_/\ \/ \ \  __\ ');
+// console.log(' \ \_\ \ \_\  \ \_____\  \ \_____\    \ \_\  \ \_\   ');
+// console.log('  \/_/  \/_/   \/_____/   \/_____/     \/_/   \/_/   ');
+// console.log(' --------------------------------------------------');
